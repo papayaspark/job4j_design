3cd9d2c365a001dfc3183e6d190eacb4012c4e7c
Lalina Kairova <klea0840@gmail.com>
2021-07-11 16:22:44 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/collection/ForwardLinked.java
@@ -0,0 +1,70 @@
+package collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLinked <T> implements Iterable<T> {
+    private Node<T> head;
+    int count = 0;
+    int modCount = 0;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    public T deleteFirst() {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        Node<T> first = head;
+        final T element = first.value;
+        final Node<T> next = first.next;
+        first.value = null;
+        first.next = null;
+        head = next;
+        count--;
+        modCount++;
+        return element;
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
