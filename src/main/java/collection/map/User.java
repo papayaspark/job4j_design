0d44c319c0f0e28538ae865478f70c9285fb9ca0
Lalina Kairova <klea0840@gmail.com>
2021-07-14 22:38:02 +0300

    1. Создать модель User [#999]

---
+++ src/main/java/collection/map/User.java
@@ -0,0 +1,15 @@
+package collection.map;
+
+import java.util.Calendar;
+
+public class User {
+    String name;
+    int children;
+    Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.birthday = birthday;
+        this.children = children;
+    }
+}
