00ce7bf8552da875defbaf7deb6e28303064e0e3
Lalina Kairova <klea0840@gmail.com>
2021-07-18 17:57:41 +0300

    1. Создать элементарную структуру дерева [#1711]

---
+++ src/main/java/collection/tree/SimpleTree.java
@@ -0,0 +1,44 @@
+package collection.tree;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
+
+public class SimpleTree<E> implements Tree<E> {
+    private final Node<E> root;
+
+    public SimpleTree(final E root) {
+        this.root = new Tree.Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rsl = false;
+        Optional<Node<E>> parents = findBy(parent);
+        Optional<Node<E>> kid = findBy(child);
+        if(parents.isPresent()) {
+            if(kid.isEmpty()) {
+                Node<E> gotKid = new Node<>(child);
+                parents.get().children.add(gotKid);
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/collection/tree/Tree.java
@@ -0,0 +1,29 @@
+package collection.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public interface Tree<E> {
+
+    boolean add(E parent, E child);
+
+    Optional<Node<E>> findBy(E value);
+
+    class Node<E> {
+        final E value;
+        final List<Node<E>> children = new ArrayList<>();
+
+        public Node(E value) {
+            this.value = value;
+        }
+
+        public E getValue() {
+            return value;
+        }
+
+        public List<Node<E>> getChildren() {
+            return children;
+        }
+    }
+}
