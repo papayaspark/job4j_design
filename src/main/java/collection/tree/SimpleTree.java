06ecfc7cf3f184c65f5f66bf311c090698187377
Lalina Kairova <klea0840@gmail.com>
2021-07-20 00:05:11 +0300

    2. Добавить метод boolean isBinary() [#1712 #179036] corr

--- src/main/java/collection/tree/SimpleTree.java
+++ src/main/java/collection/tree/SimpleTree.java
@@ -3,6 +3,7 @@ package collection.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -28,30 +29,26 @@ public class SimpleTree<E> implements Tree<E> {
 
     @Override
     public Optional<Node<E>> findBy(E value) {
+        Predicate<Node<E>> filter = eNode -> eNode.value.equals(value);
+        return findByPredicate(filter);
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> filter = eNode -> eNode.children.size() > 2;
+        return findByPredicate(filter).isEmpty();
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
-        }
-        return rsl;
-    }
-
-    public boolean isBinary() {
-        boolean rsl = true;
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.children.size() > 2) {
-                rsl = false;
-                break;
-            }
         }
         return rsl;
     }
