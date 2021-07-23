9c7aea3bd69f4931d9ad272333f12e6cf358cddf
Lalina Kairova <klea0840@gmail.com>
2021-07-23 20:35:35 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/io/File.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.FileOutputStream;
+
+public class File {
+    public static void main(String[] args) {
+        int line, col, rsl;
+                try (FileOutputStream out = new FileOutputStream("result.txt")) {
+                    for (line = 1; line < 11; line++) {
+                        System.out.println();
+                        for (col = 1; col < 11; col++) {
+                            rsl = line * col;
+                            System.out.print(rsl + " ");
+                            out.write(rsl);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+        }
+    }
+}
