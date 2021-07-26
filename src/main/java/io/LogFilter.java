3a53ccc33c821fa52d36a5ad487030e533f750ca
Lalina Kairova <klea0840@gmail.com>
2021-07-26 23:43:11 +0300

    0.4. BufferedOutputStream [#252490]

--- src/main/java/io/LogFilter.java
+++ src/main/java/io/LogFilter.java
@@ -1,7 +1,6 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,9 +18,22 @@ public class LogFilter {
         return list;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            log.forEach(s -> out.write(s + System.lineSeparator()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
         System.out.println(log.size());
+        save(log, "404.txt");
     }
 }
