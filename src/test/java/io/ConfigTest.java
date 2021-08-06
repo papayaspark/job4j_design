1d7c63a331cd32fb6e7b00cae4a0e2cd80d2a558
Lalina Kairova <klea0840@gmail.com>
2021-08-06 15:51:18 +0300

    1. Читаем файл конфигурации [#858 #184486]

---
+++ src/test/java/io/ConfigTest.java
@@ -0,0 +1,18 @@
+package io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+}
