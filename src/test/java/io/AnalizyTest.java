db2c4bb96155f0b250d4adf2ca6088ad564199ff
Lalina Kairova <klea0840@gmail.com>
2021-08-10 23:59:55 +0300

    3.0. Тестирование IO [#173905 #187418] corr

--- src/test/java/io/AnalizyTest.java
+++ src/test/java/io/AnalizyTest.java
@@ -20,12 +20,12 @@ public class AnalizyTest {
         java.io.File source = folder.newFile("source.txt");
         java.io.File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("200 10:56:01\n"
-                    + "500 10:57:01\n"
-                    + "400 10:58:01\n"
-                    + "500 10:59:01\n"
-                    + "400 11:01:02\n"
-                    + "200 11:02:02");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("200 11:02:02");
         }
         Analizy anz = new Analizy();
         anz.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
